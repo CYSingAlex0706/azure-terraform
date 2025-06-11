14968c328c9b035acb9a5d5b3e4eb93708f882a5
Alex Chui <alexchui@chinachemgroup.com>
2025-06-11 17:57:46 +0800

    key vault

--- main.tf
+++ main.tf
@@ -2,3 +2,16 @@ resource "azurerm_resource_group" "rg" {
   name     = "tf-test-123"
   location = "East Asia"
 }
+
+resource "azurerm_key_vault" "example" {
+  name                        = "example-kv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_enabled         = true
+  purge_protection_enabled    = false
+  enable_rbac_authorization   = false
+
+}
+
---
+++ varables.tf
@@ -0,0 +1,15 @@
+variable "TFC_AZURE_RUN_CLIENT_ID" {
+  type = string
+}
+
+variable "TFC_AZURE_PROVIDER_AUTH" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
