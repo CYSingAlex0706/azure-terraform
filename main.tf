c84289ff4c5e4fe5456c8d853733078f88ed3ff4
CYSingAlex0706 <97864075+CYSingAlex0706@users.noreply.github.com>
2025-06-16 23:05:11 +0800

    update

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_service_plan" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "example" {
