dc43da4b7f5d3f20bf0637ed2e7039105416bc2b
CYSingAlex0706 <97864075+CYSingAlex0706@users.noreply.github.com>
2025-06-16 22:53:10 +0800

    update

--- main.tf
+++ main.tf
@@ -14,3 +14,22 @@ resource "azurerm_key_vault" "example" {
 
 }
 
+resource "azurerm_service_plan" "example" {
+  name                = "test-app-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "test"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+  app_settings        = {
+    "Key" = "Value"
+  }
+
+  site_config {}
+}
